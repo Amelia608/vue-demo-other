9bf63412025a0659f974e9dfe32b1c89a1991cb1
chenying <chenying@wowkai.cn>
2020-04-11 11:41:30 +0800

    mock数据  vue.config配置共享全局变量，mixins

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,55 +1,27 @@
 <template>
-    <div>
-        <h1>mock数据查询</h1>
-        <el-form :inline="true" :model="formInline" class="demo-form-inline">
-            <el-form-item label="页码">
-                <el-input
-                    v-model="formInline.page"
-                    placeholder="请输入"
-                ></el-input>
-            </el-form-item>
-            <el-form-item label="数量">
-                <el-input
-                    v-model="formInline.count"
-                    placeholder="请输入"
-                ></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="getList">查询</el-button>
-            </el-form-item>
-        </el-form>
-        <pre v-html="JSON.stringify(list, null, 2)"></pre>
+    <div class="app-page-wrapper">
+       <side-bar/>
+        <div class="app-main-wrap">
+          <nav-bar/>
+        </div>
     </div>
 </template>
 <script>
-import axios from "axios";
+import NavBar from './components/NavBar'
+import SideBar from './components/SideBar'
 export default {
+    components: { NavBar, SideBar },
     data() {
-        return {
-            formInline: {
-                page: 1,
-                count: 10,
-            },
-            list: [],
-        };
+        return {};
     },
     watch: {},
-    mounted(){
-      this.getList()
-    },
-    methods: {
-        getList() {
-            axios
-                .get("http://your.domain.com/test", {
-                    params: {
-                        page: this.formInline.page,
-                        count: this.formInline.count,
-                    },
-                })
-                .then(({data}) => {
-                    this.list = data.data;
-                });
-        },
-    },
+    methods: {}
 };
 </script>
+<style lang="scss" scoped>
+.app-page-wrapper{
+ position: relative;
+ padding-left: 260px;
+ 
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,18 @@ module.exports={
   publicPath:'/',
   //构建时的输出目录
   outputDir:'dists',
+  css: {
+    loaderOptions: {
+      // 向所有 Sass 样式传入共享的全局变量、mixins
+      sass: {
+        prependData: `
+          @import "@/styles/_variables.scss";
+          @import "@/styles/_variables-element.scss";
+          @import "@/styles/_mixins.scss";
+        `
+      }
+    }
+  },
   //放置静态资源
   assetsDir:'static',
   indexPath:'index.html'
