0b8364c16e04398debfb3371a963db7e765aa6a5
chenying <wb.chen.ying@redstarclouds.com>
2020-04-28 11:16:54 +0800

    moment 过滤器and全局注册到vue2种使用方法

--- src/main.js
+++ src/main.js
@@ -1,22 +1,27 @@
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import countTo from 'vue-count-to'
-import 'element-ui/lib/theme-chalk/index.css';
-import './styles/index.scss';
-import App from './App.vue'
-import router from './router'
-import './permission'
-import variables from '@/styles/_variables-element.scss'
-import store from '@/store'
+import Vue from "vue";
+import ElementUI from "element-ui";
+import countTo from "vue-count-to";
+import moment from "moment";
+import "element-ui/lib/theme-chalk/index.css";
+import "./styles/index.scss";
+import App from "./App.vue";
+import router from "./router";
+import "./permission";
+import variables from "@/styles/_variables-element.scss";
+import store from "@/store";
 // console.log(Vue.config.productionTip,process.env.NODE_ENV)
-const version=require('element-ui/package.json').version
-console.log(variables,version)
-Vue.component('countTo',countTo)
-Vue.use(ElementUI)
-Vue.config.productionTip = false
-process.env.NODE_ENV==='development'?require('./mock/api'):''
+const version = require("element-ui/package.json").version;
+console.log(variables, version);
+Object.defineProperty(Vue.prototype, "$moment", { value: moment });
+Vue.component("countTo", countTo);
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+Vue.filter('convertDate',function(value){
+	return moment(value).format('YYYY-MM-DD');
+});
+process.env.NODE_ENV === "development" ? require("./mock/api") : "";
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   store,
-  router
-}).$mount('#app')
+  router,
+}).$mount("#app");
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,22 +1,33 @@
 
 <template>
-  <div>
-    <countTo class="countto" :startVal='startVal' :endVal='endVal' :duration='3000'/>
-  </div>
+	<div>
+		<countTo class="countto"
+			:startVal='startVal'
+			:endVal='endVal'
+			:duration='3000'
+			prefix="$" />
+		<p class="mt20">发表时间:{{date | convertDate}}</p>
+    <p class="mt20">formateDate:{{formatDate||'--'}}</p>
+	</div>
 </template>
 <script>
 export default {
-  data(){
-    return {
-      startVal:0,
-      endVal:1000
-    }
-  }
+	data () {
+		return {
+			startVal: 0,
+			endVal: 1000,
+      date: new Date(),
+      formatDate:''
+		}
+	},
+	mounted () {
+    this.formatDate=this.$moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+	}
 }
 </script>
 <style lang="scss" scoped>
-  .countto{
-    font-size: 20px;
-    color:$--color-primary;
-  }
+.countto {
+	font-size: 20px;
+	color: $--color-primary;
+}
 </style>
