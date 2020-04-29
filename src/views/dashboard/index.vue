09ed9e9ca6505603abaad14d9a63efbe9f98393a
chenying <wb.chen.ying@redstarclouds.com>
2020-04-29 14:01:00 +0800

    nextTick异步问题验证 图片加载失败添加缺省图或者icon

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,4 +1,3 @@
-
 <template>
 	<div>
 		<countTo class="countto"
@@ -7,21 +6,45 @@
 			:duration='3000'
 			prefix="$" />
 		<p class="mt20">发表时间:{{date | convertDate}}</p>
-    <p class="mt20">formateDate:{{formatDate||'--'}}</p>
+		<p class="mt20">formateDate:{{formatDate||'--'}}</p>
+		<hr class="mt20 mb20">
+		<div>
+			<span class="mr10"
+				href="MM"
+				id="msg">{{msg}}</span>
+			<el-button type="primary"
+				@click="updateMsg">更新MSG</el-button>
+		</div>
+		<hr class="mt20 mb20">
+		<div class="img-box">
+			<img src="url"	alt="">
+		</div>
 	</div>
 </template>
 <script>
 export default {
 	data () {
 		return {
+			msg: '未更新',
 			startVal: 0,
 			endVal: 1000,
-      date: new Date(),
-      formatDate:''
+			date: new Date(),
+			formatDate: '',
+			url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588149115879&di=3b607c40d7b7798b72dab517828604e8&imgtype=0&src=http%3A%2F%2Fuploads.5068.com%2Fallimg%2F1712%2F151-1G2060U603.jpg'
 		}
 	},
 	mounted () {
-    this.formatDate=this.$moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+		this.formatDate = this.$moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+	},
+	methods: {
+		updateMsg () {
+			this.msg = '更改数据'
+			let msgDom = document.getElementById('msg')
+			console.log(msgDom.textContent)
+			this.$nextTick().then(() => {
+				console.log(msgDom.textContent)
+			})
+		}
 	}
 }
 </script>
@@ -29,5 +52,30 @@ export default {
 .countto {
 	font-size: 20px;
 	color: $--color-primary;
+}
+.img-box {
+	width: 100px;
+	height: 100px;
+  border: 1px solid #f4f4f4;
+  border-radius: 3px;
+	position: relative;
+	img {
+    max-width: 100%;
+    max-height: 100%;
+		&::after {
+      font-family: "iconfont" !important;
+			content: "\ec0d";
+      position: absolute;
+      font-size: 40px;
+      color: #ccc;
+			width: 100%;
+			height: 100%;
+      top: 0;
+      left: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+		}
+	}
 }
 </style>
